b06e908a7d456181ecba7803878a4bb2e3bb4c26
Sourav Kumar <95kumarsourav>
2020-03-30 11:31:34 +0530

    intial

---
+++ EmpRepo/EmpRepo/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace EmpRepo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EmpRepo.EmpContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EmpRepo.EmpContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ EmpRepo/EmpRepo/Repository/Context/EmpContext.cs
@@ -0,0 +1,15 @@
+﻿using EmpRepo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EmpRepo
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext() : base("conStr") { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
